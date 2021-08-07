c7ed9684a0f18fd2160066416e94b9f33b2c3559
yosho_cho <choyosho165152987@gmail.com>
2021-08-07 18:00:34 +0900

    playerのHPが減る時や、HPが0になったときのイベントを追加、player1の勝利条件を作成

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UniRx;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] SceneChangeFade m_sceneChange;
 
+    [SerializeField] Text m_player1Text = default;
+
+    [SerializeField] Text m_player2Text = default;
+
+    [SerializeField] Player1HP m_player1Hp;
 
     void Start()
     {
-        
+        m_player1Hp.PlayerDeth
+             .Subscribe(_ => Player2Win());
     }
 
-    // Update is called once per frame
-    void Update()
+
+   
+    void Player1Win()
     {
-        
+        m_player1Text.text = "Win!!";
+        m_player2Text.text = "Lose....";
     }
+
+   void Player2Win()
+    {
+        m_player2Text.text = "Win!!";
+        m_player1Text.text = "Lose....";
+    }
+
 }
